423d79eed0f0365f0907f774ed720ef29c23b38e
ShreshthaPratapSingh <shreshthapratap0@gmail.com>
2025-10-24 18:16:23 +0530

    Create Login page and make it interactive

---
+++ LogIn.js
@@ -0,0 +1,12 @@
+let getLogInBTN = document.getElementsByClassName("LogIn")[0];
+let getLogInOverlay = document.getElementsByClassName("LogInSector")[0];
+let getClose = document.getElementsByClassName("cross")[0];
+
+getLogInBTN.addEventListener("click", function(){
+    document.body.style.overflowY = "hidden";
+    getLogInOverlay.style.display = "flex";
+})
+getClose.addEventListener("click", function(){
+    document.body.style.overflowY = "visible";
+    getLogInOverlay.style.display = "none"
+})
