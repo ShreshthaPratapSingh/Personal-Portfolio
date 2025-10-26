c3e02b3fb7299303fe2c5af265eb24ffa6095bde
ShreshthaPratapSingh <shreshthapratap0@gmail.com>
2025-10-26 21:22:14 +0530

    Complete Hiring page and connected with backend database

--- hiringPage.js
+++ hiringPage.js
@@ -1,10 +1,33 @@
 let getHiringButton = document.getElementsByClassName("HireMe")[0];
 let getHiringSector = document.getElementsByClassName("HiringSector")[0];
 let getRemoveButton = document.getElementsByClassName("crossHir")[0];
+let getHiringForm = document.getElementsByClassName("HiringForm")[0];
 
 getHiringButton.addEventListener("click", function(){
     getHiringSector.style.display = "flex";
 })
 getRemoveButton.addEventListener("click", function(){
     getHiringSector.style.display = "none";
+})
+getHiringForm.addEventListener("submit", async(e)=>{
+    e.preventDefault();
+
+    const formdata = {
+        role: e.target.role.value,
+        name: e.target.name.value,
+        email: e.target.email.value,
+        number: e.target.number.value,
+        organization: e.target.organization.value
+    }
+    const send = await fetch("http://localhost:3000/send-Hform-data", {
+        method: "POST",
+        headers: {"Content-type":"application/json"},
+        body: JSON.stringify(formdata)
+    })
+    const response_Hire = await send.json();
+    console.log(response_Hire)
+    if(response_Hire.success){
+        getHiringSector.style.display = "none";
+        
+    }
 })
