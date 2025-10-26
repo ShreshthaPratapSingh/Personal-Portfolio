5161799b1b783da52075f25185b3278c9d868c67
ShreshthaPratapSingh <shreshthapratap0@gmail.com>
2025-10-26 16:12:07 +0530

    Create Hiring form

--- hiringPage.js
+++ hiringPage.js
@@ -1,6 +1,10 @@
 let getHiringButton = document.getElementsByClassName("HireMe")[0];
 let getHiringSector = document.getElementsByClassName("HiringSector")[0];
+let getRemoveButton = document.getElementsByClassName("crossHir")[0];
 
 getHiringButton.addEventListener("click", function(){
     getHiringSector.style.display = "flex";
+})
+getRemoveButton.addEventListener("click", function(){
+    getHiringSector.style.display = "none";
 })
